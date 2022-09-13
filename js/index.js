79fdcc622ccfcccc7b8e446142af163637b923b8
Cleyton Melo <cleytonw11@gmail.com>
2022-09-12 22:57:09 -0300

    adjusting the functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import resetControls from './controls.js';
-import { updateTimerDisplay, resetTimer, countdown } from './timer.js';
+import Controls from './controls.js';
+import Timer from './timer.js';
 
 const buttonPlay = document.querySelector('.play');
 const buttonPause = document.querySelector('.pause');
@@ -9,30 +9,34 @@ const buttonSoundOn = document.querySelector('.sound-on');
 const buttonSoundOff = document.querySelector('.sound-off');
 const minutesDisplay = document.querySelector('.minutes');
 const secondsDisplay = document.querySelector('.seconds');
-let minutes = Number(minutesDisplay.textContent);
-let timerTimeOut;
 
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
+const timer = Timer({
+  minutesDisplay, 
+  secondsDisplay, 
+  resetControls: controls.reset,
+});
 
 buttonPlay.addEventListener('click', () => {
-  buttonPlay.classList.add('hide');
-  buttonPause.classList.remove('hide');
-  buttonStop.classList.remove('hide');
-  buttonSet.classList.add('hide');
-
-  countdown();
+  controls.play()
+  timer.countdown();
 })
 
 buttonPause.addEventListener('click', () => {
-  buttonPause.classList.add('hide');
-  buttonPlay.classList.remove('hide');
-
-  clearTimeout(timerTimeOut)
+  controls.pause();
+  timer.hold();
 })
 
 buttonStop.addEventListener('click', () => {
-  resetControls()
-  clearTimeout(timerTimeOut)
-  resetTimer()
+  controls.reset()
+  timer.reset()
 })
 
 buttonSoundOn.addEventListener('click', () => {
@@ -46,12 +50,13 @@ buttonSoundOff.addEventListener('click', () => {
 })
 
 buttonSet.addEventListener('click', () => {
-  let newMinutes = prompt('Quantos minutos?') || 0;
+  let newMinutes = controls.getMinutes();
+
   if(!newMinutes) {
-    resetTimer();
+    timer.reset();
     return;
   }
 
-  minutes = newMinutes;
-  updateTimerDisplay(minutes, 0);
+  timer.updateDisplay(newMinutes, 0);
+  timer.updateMinutes(newMinutes);
 })
